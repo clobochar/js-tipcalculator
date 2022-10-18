36cac3e8fe295890bc807ba93a3f63e202eb46dd
clobochar <clo.bochar_@hotmail.com>
2022-10-18 10:42:01 +0100

    Add isZero function

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log("ola");
+const object = {
+  bill: "",
+  tip: "",
+  people: "",
+};
+
+function isZero() {
+  const billInput = document.querySelector(".bill-input");
+  const peopleInput = document.querySelector(".people-input");
+
+  if (object.bill == 0) {
+    billInput.classList.add("error");
+  } else {
+    billInput.classList.remove("error");
+  }
+
+  if (object.people == 0) {
+    peopleInput.classList.add("error");
+  } else {
+    peopleInput.classList.remove("error");
+  }
+}
+
+const updateObject = (event) => {
+  Object.assign(object, { [event.target.name]: event.target.value });
+  isZero();
+  console.log(object);
+};
